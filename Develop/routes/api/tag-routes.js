54a2decbd3700fa6516d75ef322c141d40fcac5f
samuel ezadmanesh <samezad27@gmail.com>
2023-01-17 10:09:35 -0800

    still working on tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -21,13 +21,25 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.
+  }
 });
 
 router.put("/:id", (req, res) => {
